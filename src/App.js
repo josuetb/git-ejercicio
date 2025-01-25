d6a312b01fb7d40e5a1fdc339f7bd84045742c86
Josue Tipan <jatipanb@puce.edu.ec>
2025-01-25 13:34:12 -0500

    cambio de titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Creado por Josue Tipan y Andres Brito
         </p>
         <a
           className="App-link"
